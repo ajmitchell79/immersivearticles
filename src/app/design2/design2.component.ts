e810abc92cdfa19639d9643ebe68c22e7ae94b85
AndreyReshetnikov <a_reshetnikov@yahoo.com>
2019-05-15 23:58:35 +0100

    Design 2 v1

--- src/app/design2/design2.component.ts
+++ src/app/design2/design2.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ElementScrollPercentage } from "../services/element-scroll-percentage.service";
 
 @Component({
   selector: 'app-design2',
@@ -6,10 +7,33 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./design2.component.scss']
 })
 export class Design2Component implements OnInit {
+  public hideFirst = false;
+  public hideSecond = true;
+  private elementScrollPercentage: ElementScrollPercentage;
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor(elementScrollPercentage: ElementScrollPercentage ) { 
+    this.elementScrollPercentage = elementScrollPercentage;
   }
 
+  ngOnInit() {
+    this.elementScrollPercentage
+    .getScrollAsStream() // Defaults to Document if no Element supplied.
+    .subscribe(
+        ( percent: number ) : void => {
+          console.log('page: ' + percent);
+        }
+    );
+  }
+
+  public recordInnerScroll( percent: number ) : void {
+    if(percent >= 0.0 && percent <= 50.0) {
+      this.hideFirst = false;
+      this.hideSecond = true;
+    }else{
+      this.hideFirst = true;
+      this.hideSecond = false;
+    }
+
+    console.log('first: ' + percent);
+  }
 }
